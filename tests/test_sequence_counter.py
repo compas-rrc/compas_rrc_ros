ae25ac9a8dd7e4fd85a965462819154bd76c627e
Gonzalo Casas <casas@arch.ethz.ch>
2019-02-28 02:32:05 +0100

    Add unit test

---
+++ tests/test_sequence_counter.py
@@ -0,0 +1,23 @@
+import pytest
+from abb_042_driver.message import SequenceCounter
+
+
+def test_thread_safety():
+    import threading
+
+    counter = SequenceCounter()
+    threads = []
+
+    def incrementor():
+      for _ in range(100000):
+        counter.increment()
+
+    for _ in range(4):
+      thread = threading.Thread(target=incrementor)
+      thread.start()
+      threads.append(thread)
+
+    for thread in threads:
+      thread.join()
+
+    assert counter.value == 400000
