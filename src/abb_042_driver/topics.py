311d32678eda12db6fe518820922fdd1d1396fd1
Gonzalo Casas <casas@arch.ethz.ch>
2019-08-07 11:50:34 +0200

    Ensure sequence ID ordering and remove queue to make published msgs sync

--- src/abb_042_driver/topics.py
+++ src/abb_042_driver/topics.py
@@ -1,4 +1,5 @@
 import rospy
+from threading import Lock
 from abb_042_driver import msg
 
 
@@ -6,27 +7,44 @@ class RobotMessageTopicProvider(object):
     def __init__(self, topic_name_sub, topic_name_pub, streaming_interface, robot_state):
         super(RobotMessageTopicProvider, self).__init__()
 
+        self._publish_lock = Lock()
+        self._receive_lock = Lock()
+        self._last_published_id = 0
+        self._last_received_id = 0
+
         self.streaming_interface = streaming_interface
         self.robot_state = robot_state
 
-        self.subscriber = rospy.Subscriber(topic_name_sub, msg.RobotMessage, self.callback)
-        self.publisher = rospy.Publisher(topic_name_pub, msg.RobotMessage, queue_size=10)
+        self.subscriber = rospy.Subscriber(topic_name_sub, msg.RobotMessage, self.ros_to_robot_handler)
+        self.publisher = rospy.Publisher(topic_name_pub, msg.RobotMessage)
 
-        self.robot_state.on_message(self.message_received)
+        self.robot_state.on_message(self.robot_to_ros_handler)
 
         rospy.loginfo('Topic provider started. Subscribed to %s, publishing to %s', topic_name_sub, topic_name_pub)
 
-    def callback(self, ros_message):
+    def ros_to_robot_handler(self, ros_message):
+        """Handle messages from ROS topics to the robot controller"""
         try:
-            self.streaming_interface.execute_instruction(ros_message)
+            with self._publish_lock:
+                if ros_message.sequence_id != self._last_published_id + 1:
+                    raise Exception('Received out of order (ROS -> Controller). Received={}, Last sequence id={}'.format(ros_message.sequence_id, self._last_published_id))
+                self.streaming_interface.execute_instruction(ros_message)
         except Exception as e:
             rospy.logerr(e)
+        finally:
+            self._last_published_id = ros_message.sequence_id
 
-    def message_received(self, ros_message):
-        try:
-            self.publisher.publish(ros_message)
-        except Exception as e:
-            rospy.logerr(e)
+    def robot_to_ros_handler(self, ros_message):
+        """Handle messages from the robot controller to ROS topic."""
+        with self._receive_lock:
+            try:
+                if ros_message.sequence_id != self._last_received_id + 1:
+                    raise Exception('Received out of order (Controller -> ROS). Received={}, Last sequence id={}'.format(ros_message.sequence_id, self._last_received_id))
+                self.publisher.publish(ros_message)
+            except Exception as e:
+                rospy.logerr(e)
+            finally:
+                self._last_received_id = ros_message.sequence_id
 
     def disconnect(self):
         try:
