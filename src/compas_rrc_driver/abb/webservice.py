9ed69fead360d64eb0139fc6cc58dd6948a83094
Gonzalo Casas <casas@arch.ethz.ch>
2022-02-15 15:32:27 +0100

    Update webservice.py

--- src/compas_rrc_driver/abb/webservice.py
+++ src/compas_rrc_driver/abb/webservice.py
@@ -40,7 +40,7 @@ class WebserviceInterfaceAdapter(object):
     def get_controller_state(self):
         response = self.ws.do_get('/rw/panel/ctrlstate')
         # TODO: format output as 3-value tuple
-        return response['_embedded']['_state'][0]['ctrlexecstate']
+        return response['_embedded']['_state'][0]['ctrlstate']
 
     def get_operation_mode(self):
         response = self.ws.do_get('/rw/panel/opmode')
